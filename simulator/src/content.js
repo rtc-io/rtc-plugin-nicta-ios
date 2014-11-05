fd85695ffd66eb85977f6414c26803adc1cf4e48
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-05 18:54:05 +1100

    Improvements to plugin state reporting

--- simulator/src/content.js
+++ simulator/src/content.js
@@ -1,37 +1,21 @@
 /* jshint node: true */
 'use strict';
 
-var extend = require('cog/extend');
 var enabled = parseInt(localStorage.simulatorEnabled, 10) === 1;
-var actions = {
-  toggle: toggleSimulator
-};
 var s = document.createElement('script');
 
-function toggleSimulator() {
-  localStorage.simulatorEnabled = parseInt(localStorage.simulatorEnabled, 10) ^ 1;
-}
-
-function handleMessage(evt) {
-  if (evt.data && evt.data.src === 'page') {
-    // make a request to the runtime
-    chrome.runtime.sendMessage(evt.data, function(response) {
-      window.postMessage(extend(response, { src: 'extension' }, '*'));
-    });
-  }
-}
-
 chrome.runtime.onMessage.addListener(function(data, sender) {
-  var action = actions[(data || {}).action];
-
   if (sender.tab) {
     return;
   }
 
-  if (typeof action == 'function') {
-    return action(data, sender);
+  if (data.setting) {
+    localStorage[data.setting] = data.value;
   }
 });
+
+// make a request to the runtime
+chrome.runtime.sendMessage({ action: 'checkState' });
 
 if (enabled) {
   s.src = chrome.extension.getURL('simulator.js');
@@ -41,5 +25,3 @@ if (enabled) {
 
   (document.head||document.documentElement).appendChild(s);
 }
-
-window.addEventListener('message', handleMessage);
--- simulator/src/index.js
+++ simulator/src/index.js
@@ -1,16 +1,46 @@
-/* jshint node: true */
-'use strict';
+var actions = {
+  checkState: checkState
+};
 
-function handleRequest(request, sender, sendResponse) {
-  console.log(arguments);
+function checkState(tab) {
+  var key = 'simulator:' + tab.id;
+  var enabled = parseInt(localStorage[key], 10) === 1;
+
+  console.log('enabled: ', enabled, key);
+  chrome.browserAction.setTitle({
+    title: (enabled ? 'Disable' : 'Activate') + ' the build.rtc.io iOS simulator - foobar',
+    tabId: tab.id
+  });
 }
 
-chrome.runtime.onMessage.addListener(handleRequest);
+function handleRequest(request, sender, sendResponse) {
+  var action = actions[(request || {}).action];
+
+  if (typeof action == 'function') {
+    action(sender.tab, sendResponse);
+  }
+}
+
+window.addEventListener('load', function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (chrome.runtime.lastError) {
+      return console.error(chrome.runtime.lastError);
+    }
+
+    checkState(tabs[0]);
+  });
+});
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, { action: 'toggle' });
+  var key = 'simulator:' + tab.id;
+
+  // toggle and then get the enabled state
+  localStorage[key] = parseInt(localStorage[key], 10) ^ 1;
+  chrome.tabs.sendMessage(tab.id, { setting: 'simulatorEnabled', value: localStorage[key] });
 
   setTimeout(function() {
     chrome.tabs.executeScript(tab.id, { code: 'window.location.reload();' });
-  }, 100);
+  }, 500);
 });
+
+chrome.runtime.onMessage.addListener(handleRequest);
