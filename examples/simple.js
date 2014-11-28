ee2078db6293bd4461b2823505069f85ecc60e25
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-28 14:10:18 +1100

    Refactor examples

---
+++ examples/simple.js
@@ -0,0 +1,24 @@
+// create the video target
+var crel = require('crel');
+var video = crel('video');
+
+// ensure we have a style tag that tells the video renderer what size it should be
+document.body.appendChild(crel('style', [
+  'body { margin: 0px; width: 100vw; height: 100vh; overflow: hidden }',
+  'body > * { width: 100%; height: 100%; object-fit: contain }',
+  '*[data-mirrored] { transform: scale(-1, 1); }'
+].join('\n')));
+
+// specify a plugin
+require('rtc-media')({
+  target: video,
+  maxfps: 10,
+  plugins: [
+    require('rtc-plugin-nicta-ios')
+  ]
+});
+
+// add the target to the document
+// NOTE: original video element may be removed and replaced with
+// an element suited to plugin rendering requirements (e.g. canvas)
+document.body.appendChild(video);
